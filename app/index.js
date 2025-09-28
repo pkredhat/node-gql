e198fcfab3502e070561945e57f2a99fa851420c
pkredhat <phillip.knezevich@gmail.com>
2025-09-28 01:03:57 -0400

    removed 427

--- app/index.js
+++ app/index.js
@@ -373,7 +373,7 @@ const { url } = await startStandaloneServer(server, {
   }),
 });
 
-console.log(`🚀 GraphQL 427 running at ${url}`);
+console.log(`🚀 GraphQL running at ${url}`);
 
 process.on('SIGINT', () => {
   console.log('Shutting down...');
